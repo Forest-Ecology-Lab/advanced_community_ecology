320c1ce94128479317ebe3f1aea2119cc2b99571
Zabdi Lopez <67519973+zabdilopez@users.noreply.github.com>
2026-03-08 22:37:50 +0100

    forced renv() setup :zap:

---
+++ 01_scripts/00_setup.R
@@ -0,0 +1,18 @@
+# --------------------------------- Set up Script --------------------------- *
+# Project setup for Advanced Community Ecology.
+# Run this script before running everything else in this project.
+# This installs all required packages using renv
+# --------------------------------------------------------------------------- *
+
+{
+# install renv if needed
+if (!requireNamespace("renv", quietly = TRUE)) {
+  install.packages("renv")
+}
+
+# restore project library
+cat("Restoring project environment using renv...\n")
+renv::restore(prompt = FALSE)
+
+cat("Environment ready.\n")
+}
